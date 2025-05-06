c42344e40641772ca8ff7a7fd1230a8d015705de
Lahiru_Mudith <lahimudith@gmail.com>
2025-05-06 14:45:23 +0530

    add to project section

--- demo/project/project.js
+++ demo/project/project.js
@@ -1,83 +1,35 @@
-const arrowBtns = document.querySelectorAll('.arrow-btn')
-const cardBtns = document.querySelectorAll('.card')
-let currentCard = 2;
-let dir = 1;
-moveCards()
-
-arrowBtns.forEach((btn,i)=>{
-    btn.onpointerenter = (e)=> gsap.to(btn, {
-        ease:'expo',
-        'box-shadow':'0 3px 4px #00000050'
-    })
-
-    btn.onpointerleave = (e)=> gsap.to(btn, {
-        ease:'expo',
-        'box-shadow':'0 6px 8px #00000030'
-    })
-
-    btn.onclick = (e)=> {
-        dir = (i==0)? 1:-1
-        if (i==0) {
-            currentCard--
-            currentCard = Math.max(0, currentCard)
+var swiper = new Swiper(".blog-slider", {
+    loop: false,
+    slidesPerView: "1",
+    speed: 500,
+    effect: "coverflow",
+    coverflowEffect: {
+        slideShadows: false,
+    },
+    mousewheel: {
+        invert: false,
+    },
+    autoplay: {
+        delay: 300000000,
+    },
+    breakpoints: {
+        0: {
+            effect: "slide",
+            centeredSlides: false,
+        },
+        768: {
+            slidesPerView: "2",
+            centeredSlides: true,
+        },
+        1200: {
+            slidesPerView: "3",
+            centeredSlides: true,
         }
-        else {
-            currentCard++
-            currentCard = Math.min(4, currentCard)
-        }
-        moveCards(0.75)
-    }
-})
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "fraction"
+    },
 
-cardBtns.forEach((btn,i)=>{
-    btn.onpointerenter = (e)=> gsap.to(btn, {
-        ease:'power3',
-        overwrite:'auto',
-        'box-shadow':()=>(i==currentCard)?'0 6px 11px #00000030':'0 6px 11px #00000030'
-    })
-
-    btn.onpointerleave = (e)=> gsap.to(btn, {
-        ease:'power3',
-        overwrite:'auto',
-        'box-shadow':()=>(i==currentCard)?'0 6px 11px #00000030':'0 0px 0px #00000030'
-    })
-
-    btn.onclick = (e)=> {
-        dir = (i<currentCard)? 1:-1
-        currentCard = i
-        moveCards(0.75)
-    }
-})
-
-function moveCards(dur=0){
-    gsap.timeline({defaults:{ duration:dur, ease:'power3', stagger:{each:-0.03*dir} }})
-        .to('.card', {
-            x:-270*currentCard,
-            y:(i)=>(i==currentCard)?0:15,
-            height:(i)=>(i==currentCard)?270:240,
-            ease:'elastic.out(0.4)'
-        }, 0)
-        .to('.card', {
-            cursor:(i)=>(i==currentCard)?'default':'pointer',
-            'box-shadow':(i)=>(i==currentCard)?'0 6px 11px #00000030':'0 0px 0px #00000030',
-            border:(i)=>(i==currentCard)?'2px solid #26a':'0px solid #fff',
-            background:(i)=>(i==currentCard)?'radial-gradient(100% 100% at top, #fff 0%, #fff 99%)':'radial-gradient(100% 100% at top, #fff 20%, #eee 175%)',
-            ease:'expo'
-        }, 0)
-        .to('.icon svg', {
-            attr:{
-                stroke:(i)=>(i==currentCard)?'transparent':'#36a',
-                fill:(i)=>(i==currentCard)?'#36a':'transparent'
-            }
-        }, 0)
-        .to('.arrow-btn-prev', {
-            autoAlpha:(currentCard==0)?0:1
-        }, 0)
-        .to('.arrow-btn-next', {
-            autoAlpha:(currentCard==4)?0:1
-        }, 0)
-        .to('.card h4', {
-            y:(i)=>(i==currentCard)?0:8,
-            opacity:(i)=>(i==currentCard)?1:0,
-        }, 0)
-}
+});
